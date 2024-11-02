79615e82654c4b740fec3911bdd9422f2da3af83
Edwin Cardenas <eacardenase@gmail.com>
2024-11-02 18:38:08 -0500

    refactor: use function with multiple return values and declare constant in global scope

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount, years float64
 	expectedReturnRate := 5.5
 
@@ -19,8 +20,7 @@ func main() {
 	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f", futureValue)
 	formattedFRV := fmt.Sprintf("Future Value (adjusted for Inflation): %.2f", futureRealValue)
@@ -31,4 +31,11 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	return futureValue, futureRealValue
 }
