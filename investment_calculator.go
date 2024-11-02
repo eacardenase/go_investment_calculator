435907207f06bba498aa23daf2ce0924013458d1
Edwin Cardenas <eacardenase@gmail.com>
2024-11-02 14:30:45 -0500

    feat: introducing constants

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 5.81
 	var investmentAmount float64 = 1000
 	years := 10.0
-	expectedReturnRate := 5.5
+	expectedReturnRate := 12.0
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
