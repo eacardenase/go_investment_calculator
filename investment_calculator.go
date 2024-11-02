fe93fba88ceaf0eb66d130476376f18bed0620f4
Edwin Cardenas <eacardenase@gmail.com>
2024-11-02 14:40:54 -0500

    feat: reading user input with scan and pointers introduction

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	const inflationRate = 5.81
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
 	years := 10.0
 	expectedReturnRate := 12.0
+
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
